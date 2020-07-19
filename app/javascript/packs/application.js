edee9e4e12b0c8ebcc81fc7fdd3b8863cd78b5dc
sasha370 <kng.sasha@mail.ru>
2020-07-19 08:30:18 +0300

    It1s OK!!! Install Twitter-bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("channels")
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
-
+import "bootstrap"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
