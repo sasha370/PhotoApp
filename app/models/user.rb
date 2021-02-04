d999a33a67a27b7d300756a8a3535e3b30e24ff0
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:34:14 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :validatable
+  
   has_one :payment
-  # Разделяет несколько атрибутов с моделью Payment
-  accepts_nested_attributes_for :payment
   has_many :images
+  
+  accepts_nested_attributes_for :payment
+
 end
