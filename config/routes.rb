5c0d368214ba7609d4c3a04d63298c42f1d4c57c
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-02-04 19:47:25 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :images
   devise_for :users, :controllers => {:registrations => 'registrations'}
   root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    get 'about', to: 'welcome#about'
 end
